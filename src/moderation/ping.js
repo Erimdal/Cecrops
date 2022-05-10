0f2bb46575f44e9e9d7939188c67b796f5c442e9
Julien RAYNAL <julien.raynal@etu.univ-nantes.fr>
2022-05-10 14:25:18 +0200

    fix: format change to ping

--- src/moderation/ping.js
+++ src/moderation/ping.js
@@ -13,11 +13,7 @@ module.exports = class PingCommand extends Command {
     async messageRun(message) {
         const msg = await message.channel.send('Ping?');
 
-        const content = `Pong from JavaScript! Bot Latency\
-            ${Math.round(this.container.client.ws.ping)}\
-            ms. API Latency\
-            ${msg.createdTimestamp - message.createdTimestamp}\
-            ms.`;
+        const content = `Pong from JavaScript! Bot Latency ${Math.round(this.container.client.ws.ping)} ms. API Latency ${msg.createdTimestamp - message.createdTimestamp} ms.`;
 
         return msg.edit(content);
     }
