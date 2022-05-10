a2e9127cd6cef9beb62df8410051fa1e1bbf829c
Julien RAYNAL <julien.raynal@etu.univ-nantes.fr>
2022-05-10 19:37:15 +0200

    final modifications ping

--- src/commands/moderation/ping.js
+++ src/commands/moderation/ping.js
@@ -1,7 +1,12 @@
-const {Command, RegisterBehavior} = require('@sapphire/framework');
+const {Command} = require('@sapphire/framework');
+
 const dotenv = require('dotenv');
 const fs = require('fs');
+
 const envConfig = dotenv.parse(fs.readFileSync('.env'));
+for (const k in envConfig) {
+    process.env[k] = envConfig[k];
+}
 
 module.exports = class PingCommand extends Command {
     constructor(context, options) {
