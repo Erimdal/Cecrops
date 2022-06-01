7746905fb96e02bcd3a3379747547aa67030070a
Julien RAYNAL <julien.raynal@etu.univ-nantes.fr>
2022-06-01 13:01:13 +0200

    profile

--- src/commands/games/gambling/profile.js
+++ src/commands/games/gambling/profile.js
@@ -1,7 +1,8 @@
 const {Command} = require('@sapphire/framework');
+const {MessageEmbed} = require('discord.js');
 
 const dotenv = require('dotenv');
-const fs = require('fs'); 
+const fs = require('fs');
 
 const envConfig = dotenv.parse(fs.readFileSync('.env'));
 for (const k in envConfig) {
@@ -47,6 +48,17 @@ module.exports = class ProfileCommand extends Command {
             this.chatInputRun(interaction);
         }
 
-        await interaction.reply(user);
+        const embed = new MessageEmbed()
+            .setColor('#ee6618')
+            .setTitle(`Profil de ${user.name}`)
+            .addFields([
+                {name: 'Niveau', value: `Vous êtes au niveau ${user.level}`, inline: true},
+                {name: 'Expérience / Prochain niveau', value: 'TODO', inline: true},
+                {name: 'Mise minimale', value: `${user.credits * 0.05}`, inline: true},
+                {name: 'Progression', value: 'TODO', inline: true},
+                {name: 'Total d\'expérience', value: `${user.experience}`, inline: true},
+            ]);
+
+        await interaction.reply({embeds: [embed]});
     }
 };
