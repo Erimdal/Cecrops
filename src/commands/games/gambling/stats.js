83e154a7b09e836213ce5bf180244f92c24f86b1
Julien RAYNAL <julien.raynal@etu.univ-nantes.fr>
2022-06-01 12:42:47 +0200

    lint fixes

---
+++ src/commands/games/gambling/stats.js
@@ -0,0 +1,69 @@
+const {Command} = require('@sapphire/framework');
+const {MessageEmbed} = require('discord.js');
+
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+const envConfig = dotenv.parse(fs.readFileSync('.env'));
+for (const k in envConfig) {
+    process.env[k] = envConfig[k];
+}
+
+const prisma = require('../../../prismaClient');
+const addUserGambling = require('../../../utility/addUserGambling');
+
+module.exports = class ProfileCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'stats',
+            description: 'Affiche les statistiques dans un message embed',
+        });
+    }
+
+    registerApplicationCommands(registry) {
+        registry.registerChatInputCommand(
+            (builder) =>
+                builder
+                    .setName('stats')
+                    .setDescription('Affiche les statistiques dans un message embed'),
+            {
+                guildsId: [process.env.GUILD_ID],
+            },
+        );
+    }
+
+    async chatInputRun(interaction) {
+        const clientId = parseInt(interaction.user.id);
+        const name = interaction.user.username;
+
+        const user = await prisma.users.findFirst({
+            where: {
+                clientId,
+            },
+        });
+
+        if (!user) {
+            addUserGambling(name, clientId);
+            this.chatInputRun(interaction);
+            return;
+        }
+
+        let totalProfits = 0;
+        let fields = new Array();
+
+        user.statistics.forEach((data) => {
+            totalProfits += data.profits;
+            fields.push({name: `${data.gameName}`, value: `${data.profits}`, inline: true});
+        });
+
+        fields.unshift({name: 'Total', value: `${totalProfits}`, inline: true});
+
+        const embed = new MessageEmbed()
+            .setColor('#ee6618')
+            .setTitle(`Statistiques pour ${user.name}`)
+            .addFields(fields);
+
+        await interaction.reply({embeds: [embed]});
+    }
+};
